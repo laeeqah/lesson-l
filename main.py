44c85fa9f7ff47a3c8bef24b587ce3c171c5fe1b
laeeqah <71064103+laeeqah@users.noreply.github.com>
2020-09-29 10:58:31 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+x,y,z = 1,2,3
+
+x= 1
+y= 2
+z= 3
+print(z)
+z= 4
+z= 5
+print(z)
+
+print("Hello World")
+
+radius = int(input("Enter the radius in meters"))
+_pi = 3.14
+area =_pi * radius * radius
+print("Area" , area)
+
+
+diameter = 2 * radius #diameter formula
+circumference = 2 * _pi * radius #circumference formula
+print(diameter)
+print(circumference)
+
+
+
+
+
